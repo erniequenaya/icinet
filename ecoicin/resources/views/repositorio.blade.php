e72c3aa85122b8799461ab2e034ca73ab287a873
Armando Ulloa <Armando Ulloa>
2018-09-09 19:38:38 -0300

    Actualizacion de modal en proyectos

--- ecoicin/resources/views/repositorio.blade.php
+++ ecoicin/resources/views/repositorio.blade.php
@@ -23,7 +23,7 @@
                 <h4 class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
               </div>
               <div class="mid-info mt-5">
-                
+
                 <div class="container">
                 @empty ($proyectos)
                   <fieldset class="form-group">
@@ -95,8 +95,6 @@
           </div>
       </div>
   </section>
-
-  @include('modals/añadir_proyecto')
 @endsection
 
 @section('script')
